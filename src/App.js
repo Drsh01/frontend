3d9dd1a081e11632b9a61a7f3ea1bca586e18802
Dhairya <waisbiejensn@gmail.com>
2023-08-14 17:13:37 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserDashboard from './components/UserDashboard';
 import AdminDashboard from './components/AdminDashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/user-dashboard' element={<UserDashboard />} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,7 @@ function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setError(undefined);
         const res = await axios({
             method: 'post',
             url: URL + '/login',
@@ -20,42 +21,51 @@ function Login() {
                 password: password
             }
         });
-        if (res.data === 'T') setIsAdmin(true);
-        else if (res.data === 'F') setIsAdmin(false);
+        if (res.data === 'T') {
+            setError(undefined);
+            setIsAdmin(true);
+        }
+        else if (res.data === 'F') {
+            setError(undefined);
+            setIsAdmin(false);
+        }
         else setError(res.data);
     }
 
-    if(isAdmin === true) return <Navigate to='/admin-dashboard' />
-    else if(isAdmin === false) return <Navigate to='/user-dashboard' />
+    if (isAdmin === true) return <Navigate to='/admin-dashboard' />
+    else if (isAdmin === false) return <Navigate to='/user-dashboard' />
     return (
-        <section class="text-center text-lg-start col-8 flex" >
-            <div class="card mb-3">
-                <h2 class="text-center">Login</h2>
-                <div class="row g-0 d-flex align-items-center">
-                    <div class="col-lg-4 d-none d-lg-flex">
-                        <img src="https://th.bing.com/th/id/OIP.CD2ZFlpJ2Pv_VbFyDwyDmAHaE8?pid=ImgDet&rs=1" alt="Trendy Pants and Shoes"
-                            class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="card-body py-5 px-md-5">
-                            <form className=" text-center">
-                                <div className="form-outline mb-4">
-                                    <label class="form-label" >Enter User Id</label>
-                                    <input class="form-control" type="text" required onChange={e => setUserId(e.target.value)} />
-                                </div>
-                                <div className="form-outline mb-4">
-                                    <label class="form-label">Enter Password</label>
-                                    <input class="form-control" type="password" required onChange={e => setPassword(e.target.value)} />
-                                </div>
+  
 
-                                <button type="button" class="btn btn-success btn-block col-6 mb-4" onClick={handleSubmit}>Sign in</button>
+        <div class="card mb-3" style={{ width: "70%", height: "50%", margin: "15% auto", marginBottom: "10px" }}>
+            <div class="row g-0 d-flex justify-between">
+                <div class="col-md-6">
+                    <img
+                        src="https://thumbs.dreamstime.com/b/cogs-gears-concept-coding-circuit-industrial-business-background-banner-background-vector-illustration-vector-illustration-235121277.jpg"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                        style={{ height: "100%" }}
+                    />
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <form className="text-center">
+                            <div className="form-outline mb-4">
+                                <label class="form-label" >Enter User Id</label>
+                                <input class="form-control" type="text" required onChange={e => setUserId(e.target.value)} />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label class="form-label">Enter Password</label>
+                                <input class="form-control" type="password" required onChange={e => setPassword(e.target.value)} />
+                            </div>
+                            <div className='mb-2 text-left text-danger'>{error}</div>
+                            <button type="button" class="btn btn-success btn-block col-6 mb-4" onClick={handleSubmit}>Sign in</button>
 
-                            </form>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
