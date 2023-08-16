74e435fc8af5dc36f9750faa7bbc3bbcc2a6265b
Dhairya <waisbiejensn@gmail.com>
2023-08-16 17:19:46 +0530

    changed class to className

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -1,32 +1,32 @@
 
 const AdminDashboard = () => {
   return (
-    <div class="row">
-      <div class="col-6 text-center" style={{ marginTop: "100px" }}>
+    <div className="row">
+      <div className="col-6 text-center" style={{ marginTop: "100px" }}>
           <img src="https://logodix.com/logo/1707130.png"  alt="Admin logo" />
-          <h1 class="mb-4" style={{ fontSize: "4rem" }}>Admin Dashboard</h1>
+          <h1 className="mb-4" style={{ fontSize: "4rem" }}>Admin Dashboard</h1>
           <p style={{ fontSize: "2.5rem", marginTop: "50px" }}>Admin Dashboard shows all the actions available to an admin</p>
       </div>
-      <div class="row d-flex flex-column justify-center col-6 border-left" style={{ marginTop: "90px"}}>
-        <div class="card col-8 text-center" style={{ marginLeft: "2rem" }}>
-          <div class="card-body">
-            <h3 class="card-title">Customer Data Management</h3>
-            <p class="card-text">This feature helps admin manage customers data.</p>
-            <a href="/admin/customers" class="btn btn-primary mb-4 col-6">Details</a>
+      <div className="row d-flex flex-column justify-center col-6 border-left" style={{ marginTop: "90px"}}>
+        <div className="card col-8 text-center" style={{ marginLeft: "2rem" }}>
+          <div className="card-body">
+            <h3 className="card-title">Customer Data Management</h3>
+            <p className="card-text">This feature helps admin manage customers data.</p>
+            <a href="/admin/customers" className="btn btn-primary mb-4 col-6">Details</a>
           </div>
         </div>
-        <div class="card col-8 text-center" style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>
-          <div class="card-body">
-            <h3 class="card-title">Loan Card Management</h3>
-            <p class="card-text">This feature helps admin manage loan card of the users.</p>
-            <a href='/admin/loan-card' class="btn btn-primary mb-4 col-6">Details</a>
+        <div className="card col-8 text-center" style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>
+          <div className="card-body">
+            <h3 className="card-title">Loan Card Management</h3>
+            <p className="card-text">This feature helps admin manage loan card of the users.</p>
+            <a href='/admin/loan-card' className="btn btn-primary mb-4 col-6">Details</a>
           </div>
         </div>
-        <div class="card col-8 text-center" style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>
-          <div class="card-body">
-            <h3 class="card-title">Items Master Data</h3>
-            <p class="card-text">This feature helps admin manage product details.</p>
-             <a href="/admin/items" class="btn btn-primary mb-4 col-6">Details</a>
+        <div className="card col-8 text-center" style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>
+          <div className="card-body">
+            <h3 className="card-title">Items Master Data</h3>
+            <p className="card-text">This feature helps admin manage product details.</p>
+             <a href="/admin/items" className="btn btn-primary mb-4 col-6">Details</a>
           </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light justify-end">
-            <div class="container">
-                <a class="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg fixed-top bg-light navbar-light justify-start">
+            <div className="container">
+                <a className="navbar-brand" href="/">
                     <img
                         id="LMA-logo"
                         src="https://nicholasfinancial.com/wp-content/uploads/2017/10/Cons_Loan_Icon_01.png"
@@ -11,6 +11,8 @@ export default function Navbar() {
                         height="50"
                     />
                 </a>
+                <h2>LOAN MANAGEMENT APPLICATION</h2>
+                <button style={{ opacity: 0 }}>qa4g</button>
             </div>
         </nav>
     )
