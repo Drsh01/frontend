836f1056811e3f9aba9b854d048c6f645b3a6530
vaibhav <sahni.1v@gmail.com>
2023-08-22 17:14:35 +0530

    useritemdashboard

--- src/components/User/ViewItems.js
+++ src/components/User/ViewItems.js
@@ -1,31 +1,44 @@
 const items = [
     {
         "id": 1,
-        "description": "Tea Table",
+        "description": "TEA TABLE",
         "make": "Wooden",
         "category": "Furniture",
-        "valuation": 5000
+        "valuation": 5000,
+        "image": "https://media.designcafe.com/wp-content/uploads/2021/12/27144355/design-cafe-modular-furniture-benefits.jpg"
     },
     {
         "id": 2,
-        "description": "Tea Set",
+        "description": "TEA SET",
         "make": "Glass",
         "category": "Crockery",
-        "valuation": 2000
+        "valuation": 2000,
+        "image": "https://hmhub.in/wp-content/uploads/2018/07/porcelain-3383050_1280.jpg"
     },
     {
         "id": 3,
-        "description": "Dining Set",
-        "make": "Wooden",
-        "category": "Furniture",
-        "valuation": 5000
+        "description": "GEOMETRY BOX",
+        "make": "Plastic",
+        "category": "Stationery",
+        "valuation": 1000,
+        "image": "https://nationaltoday.com/wp-content/uploads/2022/07/12-World-Stationery-Day-1200x834.jpg.webp"
     },
     {
         "id": 4,
-        "description": "Pen",
+        "description": "DINING SET",
+        "make": "Wooden",
+        "category": "Furniture",
+        "valuation": 5000,
+        "image": "https://media.designcafe.com/wp-content/uploads/2021/12/27144355/design-cafe-modular-furniture-benefits.jpg"
+        
+    },
+    {
+        "id": 5,
+        "description": "SKETCH PEN",
         "make": "Plastic",
-        "category": "Stationary",
-        "valuation": 1000
+        "category": "Stationery",
+        "valuation": 500,
+        "image": "https://nationaltoday.com/wp-content/uploads/2022/07/12-World-Stationery-Day-1200x834.jpg.webp"
     },
 ]
 
@@ -66,28 +79,23 @@ export default function ViewItems() {
             <div className="row">
                 <h2 style={{ background: 'linear-gradient(to right, #98FB98 50%, #40E0D0 50%)' }}>Items Purchased</h2>
                 <hr />
-                <table className="table table-striped table-bordered border-primary">
-                    <thead className="table-primary">
-                        <tr className="text-center">
-                            <th scope="col">Issue Id</th>
-                            <th scope="col">Item Description</th>
-                            <th scope="col">Item Make</th>
-                            <th scope="col">Item Category</th>
-                            <th scope="col">Item Valuation</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {items.map(item => <tr className="text-center">
-                            <th scope="row">{item.id}</th>
-                            <td>{item.description}</td>
-                            <td>{item.make}</td>
-                            <td>{item.category}</td>
-                            <td>{item.valuation}</td>
-                        </tr>
-                        )}
-                    </tbody>
-                </table>
-                {/* </div> */}
+                <div class="row">
+                {items.map(item =>
+                    <div class="card" style={{ width: "25rem", marginRight: "1%", marginLeft: "1%", marginBottom: "1.5%", padding: "0", backgroundColor: "cyan"}}>
+                        <img class="card-img-top img-fluid w-100 h-100" src={item.image} alt="Card image cap" style={{height: "50vw"}}></img>
+                        <div class="card-body" style={{padding: "0", fontWeight: "bold"}}>
+                            <h2 class="card-title" style={{textAlign: "center", fontFamily: "Book Antiqua", fontSize: "150%", fontWeight: "bold", color: "black", paddingTop: "5px"}}>{item.description}</h2>
+                            {/* <p class="card-text">This is the description.</p> */}
+                        </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item" style={{paddingBottom: "0", fontSize: "150%", fontFamily: "Book Antiqua", backgroundColor: "cyan"}}>Category: {item.category}</li>
+                        <li class="list-group-item" style={{paddingBottom: "0", fontSize: "150%", fontFamily: "Book Antiqua", backgroundColor: "cyan"}}>Make: {item.make}</li>
+                        <li class="list-group-item" style={{paddingBottom: "0", fontSize: "150%", fontFamily: "Book Antiqua", backgroundColor: "cyan"}}>Price: {'\u20B9'} {item.valuation}/-</li>
+                        </ul>
+                    </div>
+                )}
+                </div>
+                
             </div>
         </div>
     );
